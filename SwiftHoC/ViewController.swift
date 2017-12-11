c6a290e5df440ef9826a02e434c02da591154c49
Naufal Aros <naufalaros@gmail.com>
2017-12-11 20:48:02 +0100

    Initial Commit

---
+++ SwiftHoC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftHoC
+//
+//  Created by Naufal Aros El Morabet on 11/12/2017.
+//  Copyright © 2017 Naufal Aros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
